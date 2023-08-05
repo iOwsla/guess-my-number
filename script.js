2e4dc18f5b8c8a689b908cdd9b99e3e20280f8c0
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-08-06 01:44:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 'use strict';
 
-document.querySelector(".message")
+console.log(document.querySelector(".message").textContent);
+
+
+
+
+
+
+document.querySelector(".check").addEventListener(
+    'click', function(event) {
+        var guess = Number(document.querySelector(".guess").value);
+      
+
+        if (!guess) {
+            document.querySelector(".message").textContent = "No Number! ";
+        }
+
+
+    }
+);
